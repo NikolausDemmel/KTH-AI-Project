8494bd6b6995e2b691d4a40e8348dea379f9f0bd
Nikolaus Demmel <demmeln@in.tum.de>
2011-09-23 23:06:18 +0200

    adjusted makefiles. Ready to submit milestone.

--- client/compileAndRun_cc.sh
+++ client/compileAndRun_cc.sh
@@ -1,3 +1,6 @@
-# requires boost system library and includes for array and asio
-g++ code/client.cc -lboost_system -o client
-./client 1
+# requires (boost system library, not yet) and includes for parts of "random"
+
+# The program will allocate around 600MB of RAM for a hashtable.
+
+cd code
+make run-opt PORT=7778 BOARD=1
